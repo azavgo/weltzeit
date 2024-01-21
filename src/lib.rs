709856d27850172d3173a73b7393978c0ae06190
Alex. Z <azavgo@yahoo.com>
2024-01-21 19:24:02 +1100

    Readme file updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,39 @@
-use chrono::{DateTime, Utc, Timelike, Datelike, TimeZone};
-use chrono_tz::Tz;
-use chrono_tz::Europe::{Moscow, Budapest, Vienna, Berlin, London};
-use chrono_tz::Australia::{Sydney, Brisbane, Melbourne, Hobart, Adelaide, Darwin, Perth};
-use chrono_tz::US::{Pacific, Central, Eastern};
+use chrono::{DateTime, Datelike, TimeZone, Timelike, Utc};
+use chrono_tz::Australia::{Adelaide, Brisbane, Darwin, Hobart, Melbourne, Perth, Sydney};
+use chrono_tz::Europe::{Berlin, Budapest, London, Moscow, Vienna};
 use chrono_tz::Pacific::Auckland;
+use chrono_tz::Tz;
+use chrono_tz::US::{Central, Eastern, Pacific};
 
 #[derive(Debug)]
 pub enum CityList {
-    Moscow, Budapest, Vienna, Berlin, London,
-    Sydney, Brisbane, Melbourne, Hobart, Adelaide, Darwin, Perth,
-    LosAngeles, Chicago, NewYork,    
+    Moscow,
+    Budapest,
+    Vienna,
+    Berlin,
+    London,
+    Sydney,
+    Brisbane,
+    Melbourne,
+    Hobart,
+    Adelaide,
+    Darwin,
+    Perth,
+    LosAngeles,
+    Chicago,
+    NewYork,
     Auckland,
 }
 
 #[derive(Debug)]
 pub struct City {
-    city: CityList, 
-    year: i32, 
+    city: CityList,
+    year: i32,
     month: u32,
-    day: u32, 
-    hours: u32, 
-    minutes: u32, 
-    seconds: u32, 
+    day: u32,
+    hours: u32,
+    minutes: u32,
+    seconds: u32,
 }
 
 impl City {
@@ -38,59 +50,11 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
-                CityList::Budapest => {
-                    let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Budapest);
-                    Self {
-                        city: city,
-                        year: dt_city.year(),
-                        month: dt_city.month(),
-                        day: dt_city.day(),
-                        hours: dt_city.hour(),
-                        minutes: dt_city.minute(),
-                        seconds: dt_city.second(),
-                    }
-                },
-                CityList::Vienna => {
-                    let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Vienna);
-                    Self {
-                        city: city,
-                        year: dt_city.year(),
-                        month: dt_city.month(),
-                        day: dt_city.day(),
-                        hours: dt_city.hour(),
-                        minutes: dt_city.minute(),
-                        seconds: dt_city.second(),
-                    }
-                },
-                CityList::Berlin => {
-                    let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Berlin);
-                    Self {
-                        city: city,
-                        year: dt_city.year(),
-                        month: dt_city.month(),
-                        day: dt_city.day(),
-                        hours: dt_city.hour(),
-                        minutes: dt_city.minute(),
-                        seconds: dt_city.second(),
-                    }
-                },
-                CityList::London => {
-                    let dt_city: DateTime<Tz> = Utc::now().with_timezone(&London);
-                    Self {
-                        city: city,
-                        year: dt_city.year(),
-                        month: dt_city.month(),
-                        day: dt_city.day(),
-                        hours: dt_city.hour(),
-                        minutes: dt_city.minute(),
-                        seconds: dt_city.second(),
-                    }
-                },
-            CityList::Sydney => {
-                let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Sydney);
+            }
+            CityList::Budapest => {
+                let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Budapest);
                 Self {
-                    city: city, 
+                    city: city,
                     year: dt_city.year(),
                     month: dt_city.month(),
                     day: dt_city.day(),
@@ -98,7 +62,55 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
+            CityList::Vienna => {
+                let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Vienna);
+                Self {
+                    city: city,
+                    year: dt_city.year(),
+                    month: dt_city.month(),
+                    day: dt_city.day(),
+                    hours: dt_city.hour(),
+                    minutes: dt_city.minute(),
+                    seconds: dt_city.second(),
+                }
+            }
+            CityList::Berlin => {
+                let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Berlin);
+                Self {
+                    city: city,
+                    year: dt_city.year(),
+                    month: dt_city.month(),
+                    day: dt_city.day(),
+                    hours: dt_city.hour(),
+                    minutes: dt_city.minute(),
+                    seconds: dt_city.second(),
+                }
+            }
+            CityList::London => {
+                let dt_city: DateTime<Tz> = Utc::now().with_timezone(&London);
+                Self {
+                    city: city,
+                    year: dt_city.year(),
+                    month: dt_city.month(),
+                    day: dt_city.day(),
+                    hours: dt_city.hour(),
+                    minutes: dt_city.minute(),
+                    seconds: dt_city.second(),
+                }
+            }
+            CityList::Sydney => {
+                let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Sydney);
+                Self {
+                    city: city,
+                    year: dt_city.year(),
+                    month: dt_city.month(),
+                    day: dt_city.day(),
+                    hours: dt_city.hour(),
+                    minutes: dt_city.minute(),
+                    seconds: dt_city.second(),
+                }
+            }
             CityList::Brisbane => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Brisbane);
                 Self {
@@ -110,7 +122,7 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Melbourne => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Melbourne);
                 Self {
@@ -122,7 +134,7 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Hobart => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Hobart);
                 Self {
@@ -134,7 +146,7 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Adelaide => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Adelaide);
                 Self {
@@ -146,7 +158,7 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Darwin => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Darwin);
                 Self {
@@ -158,7 +170,7 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Perth => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Perth);
                 Self {
@@ -170,11 +182,11 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::LosAngeles => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Pacific);
                 Self {
-                    city: city, 
+                    city: city,
                     year: dt_city.year(),
                     month: dt_city.month(),
                     day: dt_city.day(),
@@ -182,11 +194,11 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Chicago => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Central);
                 Self {
-                    city: city, 
+                    city: city,
                     year: dt_city.year(),
                     month: dt_city.month(),
                     day: dt_city.day(),
@@ -194,11 +206,11 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::NewYork => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Eastern);
                 Self {
-                    city: city, 
+                    city: city,
                     year: dt_city.year(),
                     month: dt_city.month(),
                     day: dt_city.day(),
@@ -206,11 +218,11 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
             CityList::Auckland => {
                 let dt_city: DateTime<Tz> = Utc::now().with_timezone(&Auckland);
                 Self {
-                    city: city, 
+                    city: city,
                     year: dt_city.year(),
                     month: dt_city.month(),
                     day: dt_city.day(),
@@ -218,7 +230,7 @@ impl City {
                     minutes: dt_city.minute(),
                     seconds: dt_city.second(),
                 }
-            },
+            }
         }
     }
 
@@ -251,26 +263,36 @@ impl City {
     }
 
     pub fn current_time(&self) -> String {
-        format!("{:?}: {}/{}/{} {}:{}:{}", self.city(), self.day(), self.month(), self.year(), self.hours(), self.minutes(), self.seconds())
+        format!(
+            "{:?}: {}/{}/{} {}:{}:{}",
+            self.city(),
+            self.day(),
+            self.month(),
+            self.year(),
+            self.hours(),
+            self.minutes(),
+            self.seconds()
+        )
     }
 
     pub fn timestamp(&self) -> i64 {
-        let dt: DateTime<Utc> = 
-            Utc.with_ymd_and_hms(self.year(), 
-                                self.month(), 
-                                self.day(), 
-                           self.hours(), 
-                            self.minutes(),
-                            self.seconds())
+        let dt: DateTime<Utc> = Utc
+            .with_ymd_and_hms(
+                self.year(),
+                self.month(),
+                self.day(),
+                self.hours(),
+                self.minutes(),
+                self.seconds(),
+            )
             .unwrap();
         dt.timestamp()
     }
 
-    pub fn time_difference(&self, city: &City) -> f32 {   
+    pub fn time_difference(&self, city: &City) -> f32 {
         let diff = city.timestamp() - self.timestamp();
-        (diff as f32)/(3600.0 as f32)
+        (diff as f32) / (3600.0 as f32)
     }
-
 }
 
 #[cfg(test)]
@@ -279,7 +301,7 @@ mod tests {
 
     #[test]
     fn test_time_difference_01() {
-        let sydney = City::new(CityList::Sydney);   
+        let sydney = City::new(CityList::Sydney);
         let auckland = City::new(CityList::Auckland);
         assert_eq!(sydney.time_difference(&auckland), 2.0);
     }
